d38fdf52cf73d51e2e028c34a1e41c75cb317e39
paulemilebec <paulemilebecquart@gmail.com>
2026-04-15 22:20:06 +0200

    pour eviter les bugs

--- server/main.py
+++ server/main.py
@@ -18,7 +18,7 @@ def execution():
         mdp = os.getenv("MDP")
         nombreDeJours = 30 
         
-        rootTest = "C:\\Users\\pebec\\Documents\\Projets perso\\Scrap edt\\server\\"
+        # rootTest = "C:\\Users\\pebec\\Documents\\Projets perso\\Scrap edt\\server\\"
         pathIcs = "app" + "/partage"
         pathJson = "app" + "/jsonAPI"
 
